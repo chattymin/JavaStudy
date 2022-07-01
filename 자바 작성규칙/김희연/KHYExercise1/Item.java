58a742abfdc25819e2aab9e78fc29778a560be19
HeeYeon-Kim <62700196+HeeYeon-Kim@users.noreply.github.com>
2022-07-01 20:25:47 +0900

    Add files via upload

---
+++ 자바 작성규칙/김희연/KHYExercise1/Item.java
@@ -0,0 +1,14 @@
+package KHYExercise1;
+
+public class Item{
+	String number;
+	String name;
+	int price;
+	
+	public Item(String number,String name,int price)
+	{
+		this.number=number;
+		this.name=name;
+		this.price=price;
+	}
+}
