0f24b4503dcd090e5572010b089c88da6aa29a2a
HeeYeon-Kim <62700196+HeeYeon-Kim@users.noreply.github.com>
2022-07-07 11:16:14 +0900

    Add files via upload

---
+++ ManagementMaterials/김희연/KHYExercise1/ContactManager.java
@@ -0,0 +1,93 @@
+package KHYExercise1;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class ContactManager {
+	ArrayList<Contact> contacts = new ArrayList<>();
+	static Scanner scan = new Scanner(System.in);
+	// 다음 함수를 채우시오. input 파일에 있는 내용을 입력
+	void readAll() {
+		while(true)
+		{
+			String phone=scan.next();
+			if(phone.equals("end"))
+			{
+				break;
+			}
+			Contact c=new Contact(phone);
+			c.read(scan);
+			contacts.add(c);
+		}
+	}
+	void printList() {
+		if (contacts.isEmpty()) {
+			System.out.println("연락처가 비어있습니다.");
+			return;
+		}
+		int i = 0;
+		for (Contact c: contacts)
+			c.print(i++);
+	}
+	void registerContact() {
+		String phone = null;
+		System.out.print("전화번호 : ");
+		phone = scan.next();
+		if (findContact(phone) != null) {
+			System.out.println("이미 존재하는 전화번호 입니다.");
+			return;
+		}
+		Contact c = new Contact(phone);
+		c.readRegister(scan);	
+		contacts.add(c);
+	}
+	void deleteContact() {
+		printList();
+		int index = -1;
+		System.out.print("삭제할 번호: ");
+		index = scan.nextInt();
+		if (index <= 0 || index > contacts.size()) {
+			System.out.println("번호가 범위를 벗어났습니다.");
+			return;
+		}
+		contacts.remove(index-1);
+	}
+	// 다음 함수를 채우시오.
+	void modifyContact() {
+		System.out.print("수정할 번호:");
+		int index=scan.nextInt()-1;
+		if(index<0 || index>contacts.size())
+		{
+			System.out.println("번호가 범위를 벗어났습니다.");
+			return;
+		}
+		contacts.get(index).modify(scan);
+	}
+	// 다음 함수를 채우시오.
+	void searchContact() {
+		System.out.print("검색 키워드:");
+		String key=scan.next();
+		for(int i=0;i<contacts.size();i++)
+		{
+			Contact c=contacts.get(i);
+			if(c.matches(key))
+			{
+				c.print(i);
+			}
+		}
+	}
+	// findContact 함수를 만드시오.
+	String findContact(String key)
+	{
+		for(int i=0;i<contacts.size();i++)
+		{
+			Contact c=contacts.get(i);
+		
+			if(c.matches(key))
+			{
+				return key;
+			}
+		}
+		return null;
+	}
+}
---
+++ ManagementMaterials/김희연/KHYExercise1/PhoneBook.java
@@ -0,0 +1,58 @@
+package KHYExercise1;
+
+public class PhoneBook {
+	ContactManager manager = new ContactManager();
+	void run() {
+		manager.readAll();
+		runMenu();
+	}
+	
+	int inputMenu() {
+		int num;
+		while (true) {
+			System.out.println("############ 연락처 관리 ############");
+			System.out.print("(1) 연락처 출력  ");
+			System.out.print("(2) 새로 등록  ");
+			System.out.print("(3) 삭제  ");
+			System.out.print("(4) 검색  ");
+			System.out.print("(5) 수정  ");
+			System.out.print("(6) 끝내기  ");
+			System.out.print("... ");
+			num = manager.scan.nextInt();
+			if (num >= 1 && num <= 6)
+				return num;
+			System.out.println("잘못된 번호입니다. 다시 입력해 주세요...");
+		}
+	}
+	// inputMenu를 이용하여 메뉴를 입력받고 실행하도록 작성하시오.
+	void runMenu() {
+		while(true) {			
+			switch(inputMenu())
+			{
+			case 1:
+				manager.printList();
+				break;
+			case 2:
+				manager.registerContact();
+				break;
+			case 3:
+				manager.deleteContact();
+				break;
+			case 4:
+				manager.searchContact();
+				break;
+			case 5:
+				manager.modifyContact();
+				break;
+			case 6:
+				return;
+			}	
+		}
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		PhoneBook m = new PhoneBook();
+		m.run();
+	}
+}
